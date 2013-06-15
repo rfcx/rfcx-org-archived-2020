298635b436d5871529a5b4e79b03712f4b3eab9a
topherwhite <topherwhite@gmail.com>
2013-06-16 06:47:08 +0700

    version association for source

--- model/_assoc.js
+++ model/_assoc.js
@@ -3,12 +3,12 @@ exports.getAssoc = function() {
   var Assoc = { 
     hasOne: [
       { parent: "Diagnostic", child: "Spectrum", as: "Spectrum" },
-      { parent: "Spectrum", child: "Diagnostic", as: "Diagnostic" },
-      { parent: "Source", child: "Version", as: "AppVersion" }
+      { parent: "Spectrum", child: "Diagnostic", as: "Diagnostic" }
     ], 
     hasMany: [
       { parent: "Source", child: "Spectrum", as: "Spectrum" },
-      { parent: "Source", child: "Diagnostic", as: "Diagnostic" }
+      { parent: "Source", child: "Diagnostic", as: "Diagnostic" },
+      { parent: "Version", child: "Source", as: "CurrentVersion" }
     ]
   };
 

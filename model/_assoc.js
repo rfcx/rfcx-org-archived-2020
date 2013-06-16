0f4401b0a3c406f62e76c911772d59178d920b79
topherwhite <topherwhite@gmail.com>
2013-06-16 07:03:57 +0700

    Spectrum/Diagnostic Association change

--- model/_assoc.js
+++ model/_assoc.js
@@ -2,13 +2,12 @@ exports.getAssoc = function() {
 
   var Assoc = { 
     hasOne: [
-      { parent: "Diagnostic", child: "Spectrum", as: "Spectrum" },
-      { parent: "Spectrum", child: "Diagnostic", as: "Diagnostic" }
     ], 
     hasMany: [
       { parent: "Source", child: "Spectrum", as: "Spectrum" },
       { parent: "Source", child: "Diagnostic", as: "Diagnostic" },
-      { parent: "Version", child: "Source", as: "CurrentVersion" }
+      { parent: "Version", child: "Source", as: "CurrentVersion" },
+      { parent: "Diagnostic", child: "Spectrum", as: "Spectra" }
     ]
   };
 

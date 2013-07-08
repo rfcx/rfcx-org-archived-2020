da74a78e7c8c27f894d2edc533bcae8ea7bb7569
topherwhite <topherwhite@gmail.com>
2013-07-08 04:30:14 -0700

    First bootstrap assets and placeholders

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@
  */
 
 exports.index = function(req, res, process, Model){
-
+  var node_env = process.env.NODE_ENV;
   res.render("index", {
-    title: "Rainforest Connection"+process.env.NODE_ENV
+    title: "Rainforest Connection"+ ((node_env==="production") ? "" : (" ("+node_env+")")),
+    segment_io_client_id: process.env.SEGMENT_IO_CLIENT_ID,
+    bootstrap_location: (node_env==="production") ? "//netdna.bootstrapcdn.com" : "/vendor"
+
   });
 };

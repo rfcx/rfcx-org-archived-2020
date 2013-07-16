d3c73f75767cd50030effb9c1bb8d2fe04e5f612
topherwhite <topherwhite@gmail.com>
2013-07-16 15:20:30 -0700

    publish script

--- misc/publish.sh
+++ misc/publish.sh
@@ -6,8 +6,9 @@ read -p "Are you sure you would like to push this version to prod? (y/n): " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo -e "\nPushing to prod.\n"
+  git checkout master;
+  eb status;
 else
-  echo -e "\nNOT pushed to prod.\n"
+  echo -e "\nNOT pushed to prod.\n";
+  git checkout master;
 fi
-git checkout master;
-eb status;

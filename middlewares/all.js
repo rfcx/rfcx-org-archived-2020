5697ac9a5af1b1dc9c891f81ba6b5c558d349ea4
topherwhite <topherwhite@gmail.com>
2013-07-16 18:27:35 -0700

    new middleware loader

---
+++ middlewares/all.js
@@ -0,0 +1,9 @@
+exports.middlewares = {
+
+  allowCrossDomain: function(req, res, next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","X-Requested-With");
+    next();
+  }
+
+}

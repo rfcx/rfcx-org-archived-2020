71090998085d70af92b3188858e52c2735b831b2
topherwhite <topherwhite@gmail.com>
2013-08-08 03:12:02 -0700

    better script loading

--- public/cdn/js/rfcx-main.js
+++ public/cdn/js/rfcx-main.js
@@ -1,7 +1,11 @@
 var RFCX = {
   currentPage: null,
+  load: {},
   cdn: { rfcx: null, bootstrap: null },
-  windowResizeTimer: null,
+  ui: { home: {}, about: {}, get_involved: {}, media: {} },
+  reactiveUi: {},
+  mapObj: null,
+  timer: { windowResize: null, windowScroll: null },
   bodyWidth: $('.container-narrow').innerWidth(),
   overflowMarginWidth: 250,
   transitionAt: { home: 418, about: 30, get_involved: 30, media: 30 },
@@ -10,11 +14,7 @@ var RFCX = {
   addThisPubId: ''
 };
 
-RFCX.setMastheadWidth = function() {
-  var newWidth = RFCX.bodyWidth+RFCX.overflowMarginWidth+Math.floor(($('body').innerWidth()-RFCX.bodyWidth)/2);
-  $(".dynamic-crop-right").css("width",newWidth);
-  console.log("Width: "+newWidth);
-}
+
 
 RFCX.emailSubscribeSubmit = function() {
   $("#mc-embedded-subscribe-form").each(function(){
@@ -25,66 +25,32 @@ RFCX.emailSubscribeSubmit = function() {
 
 
 $(function(){
-  
-  if (RFCX.currentPage === "about") {
-    setTimeout("animateAlertsInForest();",1000);
-    initMap();
-  }
 
-  RFCX.loadScripts();
-  RFCX.setMastheadWidth();
+  for (i in RFCX.load) { RFCX.load[i](); }
+  for (i in RFCX.ui[RFCX.currentPage]) { RFCX.ui[RFCX.currentPage][i](); }
+
+  RFCX.reactiveUi.modifyMastheadWidth();
 
 });
-
-RFCX.loadScripts = function() {
-
-  $.getScript(RFCX.cdn.bootstrap+"/twitter-bootstrap/2.3.2/js/bootstrap.min.js");  
-  RFCX.stripeSetup();
-  RFCX.loadAddThis();
-}
-
-function animateAlertsInForest() {
-	$("div.screen-container div.alert-help").each(function(i){
-		$(this).delay(500*i).fadeIn(function(){
-			$(this).addClass("hint--always");
-		});
-	});
-}
-
-var rfcxMap;
-
-function initMap() {
-
-  rfcxMap = new L.Map('map-container', {
-    center:[2,60],
-    zoom: 2
-  });
-  
-  var mapUrls = {
-    tiles: 'http://a.tiles.mapbox.com/v3/rfcx.map-3tqdi8se/{z}/{x}/{y}.png?as',
-    json: 'http://rfcx.cartodb.com/api/v2/viz/67b0fa66-ee40-11e2-8244-3085a9a9563c/viz.json'
-  };
-
-  L.tileLayer(mapUrls.tiles, {
-    attribution: 'Mapbox <a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>'
-  }).addTo(rfcxMap);
-  
-  cartodb.createLayer(rfcxMap, mapUrls.json)
-    .addTo(rfcxMap).on('done', function(layer) {
-        //do stuff
-    }).on('error', function(err) {
-      alert("some error occurred: " + err);
-    });
-
-}
 
 
 $(window).resize(function(){
-    clearTimeout(RFCX.windowResizeTimer);
-    RFCX.windowResizeTimer = setTimeout("RFCX.setMastheadWidth();",100);
+  clearTimeout(RFCX.timer.windowResize);
+  RFCX.timer.windowResize = setTimeout(function(){
+    RFCX.reactiveUi.modifyMastheadWidth();
+  },100);
 });
 
 $(window).scroll(function(){
+  clearTimeout(RFCX.timer.windowScroll);
+  RFCX.timer.windowScroll = setTimeout(function(){
+    RFCX.reactiveUi.modifyFollowButtons();
+  },50);
+});
+
+
+
+RFCX.reactiveUi.modifyFollowButtons = function() {
   var scrollPosition = $(window).scrollTop();
   var socialButtonColor = "000000";
   if (scrollPosition <= RFCX.transitionAt[RFCX.currentPage]) {
@@ -93,10 +59,17 @@ $(window).scroll(function(){
     socialButtonColor = "888888";
   }
   $(".aticon-facebook, .aticon-twitter, .aticon-google_follow").css({color:"#"+socialButtonColor});
-});
+}
 
-RFCX.loadAddThis = function() {
-  if (RFCX.nodeEnv === "production") {
+RFCX.reactiveUi.modifyMastheadWidth = function() {
+  var newWidth = RFCX.bodyWidth+RFCX.overflowMarginWidth+Math.floor(($('body').innerWidth()-RFCX.bodyWidth)/2);
+  $(".dynamic-crop-right").css("width",newWidth);
+}
+
+
+
+RFCX.load.addThis = function() {
+  if (RFCX.nodeEnv === "development") {
     $.getScript("//s7.addthis.com/js/300/addthis_widget.js#pubid="+RFCX.addThisPubId, function(){
         addthis.layers({
           'theme':'transparent', 'share':{ 'position':'left', 'numPreferredServices':4 },
@@ -111,9 +84,7 @@ RFCX.loadAddThis = function() {
   }
 }
 
-
-
-RFCX.stripeSetup = function(){
+RFCX.load.stripePayments = function(){
 
   if ($("#stripe-payment-button").length > 0) {
 
@@ -138,6 +109,45 @@ RFCX.stripeSetup = function(){
       });
     });
   }
+}
+
+RFCX.load.bootstrapJs = function(){
+  $.getScript(RFCX.cdn.bootstrap+"/twitter-bootstrap/2.3.2/js/bootstrap.min.js");
+}
+
+
+RFCX.ui.about.initMap = function(){
+
+  RFCX.mapObj = new L.Map('map-container', {
+    center:[2,60],
+    zoom: 2
+  });
+  
+  var mapUrls = {
+    tiles: 'http://a.tiles.mapbox.com/v3/rfcx.map-3tqdi8se/{z}/{x}/{y}.png?as',
+    json: 'http://rfcx.cartodb.com/api/v2/viz/67b0fa66-ee40-11e2-8244-3085a9a9563c/viz.json'
+  };
+
+  L.tileLayer(mapUrls.tiles, {
+    attribution: 'Mapbox <a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>'
+  }).addTo(RFCX.mapObj);
+  
+  cartodb.createLayer(RFCX.mapObj, mapUrls.json).addTo(RFCX.mapObj)
+    .on('done', function(layer){
+
+    }).on('error', function(err){
+      console.log("an error occurred: " + err);
+    });
+}
+
+RFCX.ui.about.animateHelpCalls = function() {
+  setTimeout(function(){
+    $("div.screen-container div.alert-help").each(function(i){
+      $(this).delay(500*i).fadeIn(function(){
+        $(this).addClass("hint--always");
+      });
+    });
+  }, 1000);
 }
 
 

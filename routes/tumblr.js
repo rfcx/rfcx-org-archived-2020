eb3691895848fe377372e10cbe3dbdbe4903e2f3
topherwhite <topherwhite@gmail.com>
2013-10-09 04:50:58 -0700

    tumblr caching

--- routes/tumblr.js
+++ routes/tumblr.js
@@ -1,12 +1,14 @@
 exports.refreshTumblrCache = function(req,res) {
   var https = require('https'),
     fs = require("fs"),
-    jsonPath = "./cache/tumblr-"+process.env.TUMBLR_SUBDOMAIN+"-tmp.json",
-    jsonPathFinal = "./cache/tumblr-"+process.env.TUMBLR_SUBDOMAIN+".json" ;
+    cacheDir = "./cache",
+    jsonPath = cacheDir+"/tumblr-"+process.env.TUMBLR_SUBDOMAIN+"-tmp.json",
+    jsonPathFinal = cacheDir+"/tumblr-"+process.env.TUMBLR_SUBDOMAIN+".json" ;
   https.get("https://api.tumblr.com"
     +"/v2/blog/"+process.env.TUMBLR_SUBDOMAIN+".tumblr.com/posts"
     +"?api_key="+process.env.TUMBLR_OAUTH_CONSUMER_KEY,
     function(res) {
+      if (!fs.existsSync(cacheDir)) { fs.mkdirSync(cacheDir); }
       if (fs.existsSync(jsonPath)) { fs.unlinkSync(jsonPath); }
       res.on('data', function(d) {
         fs.appendFile(jsonPath, d, "utf-8", function(e){ if (e) throw e; });
@@ -19,5 +21,5 @@ exports.refreshTumblrCache = function(req,res) {
       });
     }).on('error', function(e) { console.error(e); }
   );
-  res.end();
+  res.send("asdf");
 };

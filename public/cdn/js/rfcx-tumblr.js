ec708b7bc01092377c7a50224c0e37273b435bda
topherwhite <topherwhite@gmail.com>
2013-10-24 01:40:32 -0700

    tumblr blog cdn assets

---
+++ public/cdn/js/rfcx-tumblr.js
@@ -0,0 +1,11 @@
+
+
+$(function(){
+    
+    var pathname = window.location.pathname;
+    $("#masonry-container .post-image").remove();
+    $("#masonry-container .large-6")
+      .removeClass("large-6").removeClass("six")
+      .addClass("large-12").addClass("twelve");
+        
+});

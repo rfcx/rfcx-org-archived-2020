4d90e0ea3ea50975d5f900713b1fa35c09eecba0
topherwhite <topherwhite@gmail.com>
2013-10-26 17:07:00 -0700

    Tumblr styles

--- public/cdn/js/rfcx-tumblr.js
+++ public/cdn/js/rfcx-tumblr.js
@@ -4,59 +4,55 @@
     } else { return str; }
   }
 
-  $(function() {
+  function loadPopularSidebar() {
     var url = '/tagged/popular/rss';
     var $list = $('#popular');
-    $.ajax({
-    url: url,
-    type: 'GET',
-    dataType: 'xml',
-    success: function(data) {
-      var $items = $(data).find('item:lt(4)');
-      $items.each( function() {
-      var $item = $(this);
-      var link = $item.children('link').text();
-      var title = $item.children('title').text();
-      var content = $item.children('description').text();
-      var date = $item.children('pubDate').text();
-      var author = $item.children('dc\\:creator').text();
-      if (link || title) {
-      $list.append($('<li class="article"><a class="title" href="'+  link +'">'+ strLimitLength(title,28) +'</a>'
-        +'<a class="author-name" href="'+  link +'">by '+author+'</a>'
-        +'<div class="rte content">'+ content + '</div></li>'));
-    } 
-  });
-  $("#popular li.article").each(function(){
-  var imgurl = $(this).find('.rte.content img:eq(0)').attr('src');
-  $(this).prepend('<a href="'+$(this).find("a.title").attr("href")+'"><div class="popimg"><img src="'+ imgurl +'" /></div></a>');
-  });
+    $.ajax({ url: url, type: 'GET', dataType: 'xml',
+      success: function(data) {
+        var $items = $(data).find('item:lt(4)');
+        $items.each( function() {
+          var $item = $(this);
+          var link = $item.children('link').text();
+          var title = $item.children('title').text();
+          var content = $item.children('description').text();
+          var date = $item.children('pubDate').text();
+          var author = $item.children('dc\\:creator').text();
+          if (link || title) {
+            $list.append($('<li class="article"><a class="title" href="'+  link +'">'+ strLimitLength(title,28) +'</a>'
+              +'<a class="author-name" href="'+  link +'">by '+author+'</a>'
+            +'<div class="rte content">'+ content + '</div></li>'));
+          } 
+        });
+        $("#popular li.article").each(function(){
+        var imgurl = $(this).find('.rte.content img:eq(0)').attr('src');
+        $(this).prepend('<a href="'+$(this).find("a.title").attr("href")+'"><div class="popimg"><img src="'+ imgurl +'" /></div></a>');
+      });
+    }});
   }
-  });
-  });
+
   
-  //Photoset grid
-  $('.photoset-container').imagesLoaded( function() {
-  $('.photoset-container').photosetGrid({
-  gutter: '5px',
-  rel: $('.photoset-container').attr('data-id'),
-  highresLinks: true,
-  onInit: function(){},
-  onComplete: function(){
-  // Show the grid after it renders
-  $('.photoset-container').attr('style', '');
-  $('.photoset-container a').colorbox({
-  photo: true,
-  scalePhotos: true,
-  maxHeight:'90%',
-  maxWidth:'90%'
-  });
-  }
-  });
-  });
+  // //Photoset grid
+  // $('.photoset-container').imagesLoaded(function(){
+  //   $('.photoset-container').photosetGrid({
+  //     gutter: '5px',
+  //     rel: $('.photoset-container').attr('data-id'),
+  //     highresLinks: true,
+  //     onInit: function(){},
+  //     onComplete: function(){
+  //       // Show the grid after it renders
+  //       $('.photoset-container').attr('style', '');
+  //       $('.photoset-container a').colorbox({
+  //         photo: true, scalePhotos: true, maxHeight:'90%', maxWidth:'90%'
+  //       });
+  //     }
+  //   });
+  // });
 
 
 $(function(){
   
+  loadPopularSidebar();
+
   var pathname = window.location.pathname;
   var container = $("#container");
   var masonryContainer = $("#masonry-container");

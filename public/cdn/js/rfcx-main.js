a8f088aa9a7dad449c0f861f4609e87634a2c12d
topherwhite <topherwhite@gmail.com>
2013-11-01 15:08:49 -0700

    removed local storage for foresight.js and lots of video ui improvements

--- public/cdn/js/rfcx-main.js
+++ public/cdn/js/rfcx-main.js
@@ -17,11 +17,7 @@ var RFCX = {
   nodeEnv: null,
   appVersion: null,
   donateAmount: 50,
-  video: { offset: [0, 0, 0], obj: null, id: null, version: null, forceYouTube: false, sizes: [
-      [1920,1080,5500], [1280,720,2500], [854,480,1100], [640,360,600]
-    ], mobileSize: 3
-  },
-  speedTest: { kB: 100, expireMinutes: 2 },
+  speedTest: { kB: 150, expireMinutes: 2 },
   snapJsObj: null,
   olark: { allow: true, excludePaths: ["/intro","/video"], displayDelay: 10 },
   scrollQueues: {
@@ -257,6 +253,7 @@ RFCX.fn.ui.about.initMap = function(){
     });
 }
 
+/*
 RFCX.fn.ui.intro.initVideo = function(){
   RFCX.fn.video.init();
   RFCX.fn.video.prepare();
@@ -266,6 +263,7 @@ RFCX.fn.ui.video.initVideo = function(){
   RFCX.fn.video.init();
   RFCX.fn.video.prepare();
 }
+*/
 
 RFCX.fn.ui.about.animateHelpCalls = function() {
   setTimeout(function(){
@@ -373,159 +371,15 @@ RFCX.setOlark = function(setOnOff) {
   }
 }
 
-RFCX.getBandwidth = function() {
-  var ratio = 1;
+RFCX.getBandwidthKb = function() {
+  var kb = 0;
   if ((typeof foresight != "undefined") && (typeof foresight.connKbps != "undefined")) {
-    ratio = (foresight.connKbps/8)/RFCX.speedTest.kB;
-    analytics.track("bandwidth_test", { label: RFCX.speedTest.kB+"KB_download", value: ratio });
-  }
-  return ratio;
-}
-
-RFCX.fn.video.init = function(){
-
-  var videoJsVersion = "4.2.2";
-  RFCX.fn.insertCss(RFCX.cdn.videoJs+"/"+videoJsVersion+"/video-js.css");
-
-  $.getScript(RFCX.cdn.videoJs+"/"+videoJsVersion+"/video.js",function(){
-    if (RFCX.cdn.videoJs.indexOf("//") == -1) { videojs.options.flash.swf = RFCX.cdn.videoJs+"/"+videoJsVersion+"/video-js.swf"; }
-    $.getScript(RFCX.cdn.rfcxVendor+"/foresight.js/2.0.0/foresight.min.js",function(){
-      $(".video-box-page").each(function(){
-        if (RFCX.renderForMobile) { RFCX.fn.video.place(this);
-        } else { $(this).click(function(){ RFCX.fn.video.setup(this); });
-        } 
-      });
-      $.getScript(RFCX.cdn.rfcxVendor+"/video.js/"+videoJsVersion+"/vjs.youtube.js");
-      $.getScript(RFCX.cdn.rfcxVendor+"/video.js/"+videoJsVersion+"/media.youtube.js");
-    });
-  });
-}
-
-RFCX.fn.video.prepare = function() {
-
-    var refBox = $("div.video-box-page");
-    $("body").append(
-        "<div class=\"video-box video-box-outer\""
-          +" data-video-id=\""+refBox.attr("data-video-id")+"\""
-          +" data-video-version=\""+refBox.attr("data-video-version")+"\""
-          +" data-video-youtube=\""+refBox.attr("data-video-youtube")+"\">"
-        +"<img src=\""+RFCX.cdn.rfcx+"/img/intro/16x9.16.gif\" class=\"rfcx-trans-50 video-box-bg\">"
-        +"<i class=\"fa fa-play-circle-o\"></i>"
-        +"</div>"
-        +"<div class=\"video-box-outer-backdrop rfcx-trans-0\"></div>");
-}
-
-RFCX.fn.video.place = function(containerObj) {
-  var jqCont = $(containerObj);
-  RFCX.video.id = jqCont.attr("data-video-id");
-  RFCX.video.version = jqCont.attr("data-video-version");
-  if (!RFCX.video.forceYouTube) {
-    var sz = RFCX.video.sizes, vidSz = sz[sz.length-1], wndw = [parseInt(jqCont.width()),parseInt(jqCont.height())], bw = RFCX.getBandwidth(),
-    posterImg = (RFCX.renderForMobile) ? "//d3gq709nndn9uy.cloudfront.net/cdn/img/intro/video-poster.512.jpg" : "";
-    // set video size based on window width (or smallest for mobile devices)
-    if (RFCX.renderForMobile) { vidSz = sz[RFCX.video.mobileSize];
-    } else { for (var i = sz.length-1; i >= 0; i--) { if ((1.2 * sz[i][0]) >= wndw[0]) { vidSz = sz[i]; break; } } }
-    // Check bandwidth against bitrate of chosen size
-    if ((bw<1) && ((bw*8*RFCX.speedTest.kB) <= vidSz[2])) {
-      alert("the video is going to play slow.");
-    }
-
-    var uriBase = "//d4bl4mvczhn5i.cloudfront.net/video/"+RFCX.video.id
-        +"/v"+RFCX.video.version+"/"+RFCX.video.id+"-v"+RFCX.video.version+".",
-        vidUri = uriBase + vidSz[1],
-        posterUri = (RFCX.renderForMobile) ? $(".video-box-page .video-box-poster").attr("src") : ""
-        ;
-
-    console.log(wndw[0]+" -> "+vidSz[0]+"x"+vidSz[1]);
-      var playerHtml = "<video id=\"rfcx-video-player\" class=\"video-js vjs-default-skin\""
-              +" controls preload=\"auto\" poster=\""+posterUri+"\""
-              +" width=\""+wndw[0]+"\" height=\""+wndw[1]+"\" style=\"width:100%;\">"
-          +"<source src=\"http://www.youtube.com/watch?v="+jqCont.attr("data-video-youtube")+"\" type=\"video/youtube\" />"
-          +"<source src=\""+vidUri+".mp4\" type=\"video/mp4\" />"
-          +"<source src=\""+vidUri+".webm\" type=\"video/webm\" />"
-          +"<source src=\""+vidUri+".flv\" type=\"video/flv\" />"
-      //    +"<source src=\""+vidUri+"."+size+".3gp\" type=\"video/3gp\" />"
-          +"<track kind=\"captions\" src=\""+RFCX.cdn.rfcx+"/vtt/"+RFCX.video.id+"/"+RFCX.video.id+".en.vtt?v="+Math.random()+"\""
-              +" srclang=\"en\" label=\"English\" default />"
-          +"</video>";
-      jqCont.html(playerHtml);
-
-      videojs("rfcx-video-player", { "techOrder": ["html5","flash","youtube"], "preload": "auto", "autoplay":true, "controls":true }).ready(function(){
-        RFCX.video.obj = this;
-        RFCX.video.obj.on("pause", function(){ console.log("video paused"); });
-        RFCX.video.obj.on("ended", function(){ RFCX.fn.video.close(); });
-        analytics.track("video_play", { label: RFCX.video.id });
-        devLog("video-loaded");
-      });
-      
-    } else {
-      jqCont.html("<iframe id=\"rfcx-video-player\" class=\"\""
-          +" src=\"http://www.youtube.com/embed/"+jqCont.attr("data-video-youtube")
-            +"?enablejsapi=1"+"&iv_load_policy=3"+"&playerapiid=rfcx-video-player"
-            +"&disablekb=1"+"&wmode=transparent"+"&controls=1"+"&showinfo=0"
-            +"modestbranding=1"+"&rel=0"+"&autoplay=1"+"&loop=0"
-            +"&origin="+encodeURIComponent(window.location.origin)
-          +jqCont.attr("data-video-youtube")+"\""
-          +" width=\""+parseInt(jqCont.width())+"\" height=\""+parseInt(jqCont.height())+"\""
-          +" style=\"width:100%;\"></iframe>")
-    }
-    RFCX.fn.video.htmlClose(jqCont);
-}
-
-RFCX.fn.video.close = function() {
-  if (RFCX.video.obj != null) {
-    var percentComplete = Math.round(100*RFCX.video.obj.currentTime()/RFCX.video.obj.duration());
-    analytics.track("video_stop", { label: RFCX.video.id, value: percentComplete });
-    RFCX.video.obj.dispose();
-    RFCX.video.obj = null;
+    var kb = Math.round(foresight.connKbps);
+    if (kb==(RFCX.speedTest.kB*8)) { kb = 0; }
+    console.log("Bandwidth measured to be "+((kb==0) ? "very high" : (kb+"Kb/s")));
+    analytics.track("bandwidth_test", { label: RFCX.speedTest.kB+"KB_download", value: kb });
   } else {
-    analytics.track("video_stop", { label: RFCX.video.id, value: null });
+    console.log("Bandwidth measurements not yet available. Defaulting to higher bandwidth (for now).");
   }
-  $(document).off("keyup");
-  var jqVideoBoxOuter = $("div.video-box-outer");
-  jqVideoBoxOuter.html("<img src=\""+RFCX.cdn.rfcx+"/img/intro/16x9.16.gif\" class=\"rfcx-trans-0 video-box-bg\" /><i class=\"fa fa-play-circle-o\"></i>");
-  $("div.video-box-outer .video-box-bg").animate({opacity:0.5},1000);
-  jqVideoBoxOuter.animate({
-    top: RFCX.video.offset[0]+"px", left: RFCX.video.offset[1]+"px", width: RFCX.video.offset[2]+"px", borderWidth: "4px"
-  },500,function(){
-    $("div.video-box-outer, div.video-box-outer-backdrop").css({display:"none"});
-    RFCX.toggleAddThis(true);
-    RFCX.setOlark(true);
-  });
-}
-
-RFCX.fn.video.htmlClose = function(jqCont) {
-  if (!RFCX.renderForMobile) {
-    jqCont.append("<a href=\"javascript:RFCX.fn.video.close()\""
-          +" title=\"Stop/Close\""
-          +" class=\"video-player-close rfcx-trans-linear hover-trans-67 rfcx-trans-33 rfcx-crnr-10 rfcx-crnr-t-off rfcx-crnr-r-off\">"
-          +"<i class=\"fa fa-times\"></i></a>");
-  }
-}
-
-RFCX.fn.video.setup = function(videoBox) {
-
-    $("div.video-box-page").each(function(){
-      var gPos = $(this).offset();
-      RFCX.video.offset = [gPos.top, gPos.left, parseInt($(this).width())];
-    });
-
-    var jqVideoBoxOuter = $("div.video-box-outer");
-    jqVideoBoxOuter.css({ top: RFCX.video.offset[0]+"px", left: RFCX.video.offset[1]+"px", width:RFCX.video.offset[2]+"px", display:"block" });
-    RFCX.toggleAddThis(false);
-    $("body").animatescroll({scrollSpeed:500});
-    $(".video-box-outer-backdrop").css({display:"block",opacity:0}).animate({
-      opacity:1
-    },function(){
-      RFCX.setOlark(false);
-      $("div.video-box-outer .video-box-bg").animate({opacity:0},1000);
-      jqVideoBoxOuter.animate({
-        top: "0px", left: "0%", width: "100%", borderWidth: "0px"
-      },500,function(){
-        RFCX.fn.video.place(this);
-        $(document).keyup(function(e) {
-          if (e.keyCode == 27) { RFCX.fn.video.close(); }
-        });
-      });
-    });
+  return kb;
 }

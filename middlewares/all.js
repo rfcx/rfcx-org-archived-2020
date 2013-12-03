a553a5fb1427af623796a3c596e202621c2eefcd
topherwhite <topherwhite@gmail.com>
2013-12-02 18:37:58 -0800

    domain redirect correction

--- middlewares/all.js
+++ middlewares/all.js
@@ -4,6 +4,21 @@ exports.middlewares = {
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Headers","X-Requested-With");
     next();
+  },
+
+  productionDomainCorrection: function(req, res, next) {
+    if (  (process.env.NODE_ENV==="production")
+      &&  (req.host!=="rfcx.org")
+//      &&  (req.path.substr(0,8)!=="/vendor/")
+//      &&  (req.path.substr(0,5)!=="/cdn/")
+      &&  (req.host!=="rainforestconnection.org")
+      ) {
+        var protocol = req.protocol.toLowerCase();
+        res.writeHead(302, { "Location": protocol+"://rfcx.org"+req.path } );
+        res.end();      
+    } else {
+      next();
+    }
   }
 
 }

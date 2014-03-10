e273cd8e3de5d2d65750c2cdcf570acf43b9c80e
topherwhite <topherwhite@gmail.com>
2014-03-10 10:05:23 -0700

    blah

--- routes/index.js
+++ routes/index.js
@@ -40,8 +40,8 @@ function setJadeVars(process, jV) {
   jV.googlelibs_cdn = inProd ? "//ajax.googleapis.com/ajax/libs" : "/vendor";
   jV.videojs_cdn = inProd ? "//vjs.zencdn.net" : "/vendor/video.js"
   jV.cdnjs_cdn = inProd ? "//cdnjs.cloudflare.com/ajax/libs" : "/vendor";
-  jV.rfcx_cdn = inProd ? "//d3gq709nndn9uy.cloudfront.net/cdn" : "/cdn";
-  jV.rfcx_vendor_cdn = inProd ? "//d3gq709nndn9uy.cloudfront.net/vendor" : "/vendor";
+  jV.rfcx_cdn = inProd ? "//d3c83qz125mff6.cloudfront.net/cdn" : "/cdn";
+  jV.rfcx_vendor_cdn = inProd ? "//d3c83qz125mff6.cloudfront.net/vendor" : "/vendor";
   jV.rfcx_static_cdn = inProd ? "//d4bl4mvczhn5i.cloudfront.net" : "//rfcx-static.s3.amazonaws.com";
   jV.nav_items = navItems;
   jV.social_media = socialMedia;

036340c0ef00f0eaff9c9c8179d4cc0397dd2e22
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2015-10-16 22:35:46 +0300

    add edit button to every row in a table; add other columns

--- public/cdn/js/donations-admin.js
+++ public/cdn/js/donations-admin.js
@@ -1,23 +1,43 @@
-$(function() {
+$(function () {
 
   $('#loadingContainer').fadeOut();
 
   var table = $('#dataTable').DataTable({
+    fixedColumns: {
+      leftColumns: 2
+    },
+    "order": [[ 1, "asc" ]],
     "processing": true,
     "scrollX": true,
     "ajax": "mailchimp/get",
     "columns": [
-      { "data": "email" },
-      { "data": "merges.NAME_DONOR" },
-      { "data": "merges.COUNT" },
-      { "data": "merges.NOTE_DONOR" },
-      { "data": "merges.NOTE_ADMIN" },
-      { "data": "merges.ADDRESS" },
-      { "data": "merges.PHOTO" },
-      { "data": "merges.EMAIL_REAL" }
+      {
+        "mData": null,
+        "bSortable": false,
+        "mRender": function(data, type, full) {
+          return '<a class="fa fa-edit js-edit-row" data-id="' + data.id +'" href="javascript:void(0);"></a>';
+        }
+      },
+      {"data": "email"},
+      {"data": "merges.NAME_DONOR"},
+      {"data": "merges.COUNT"},
+      {"data": "merges.NOTE_DONOR"},
+      {"data": "merges.NOTE_ADMIN"},
+      {"data": "merges.ADDRESS"},
+      {"data": "merges.PHOTO"},
+      {"data": "merges.EMAIL_REAL"},
+      {"data": "merges.REGISTERED"},
+      {"data": "merges.RECEIVED"},
+      {"data": "merges.NAME_ADMIN"},
+      {"data": "merges.VALUE_USD"}
     ]
   });
 
-  new $.fn.dataTable.FixedColumns(table);
+  $('#dataTable').on('click', '.js-edit-row', onChangeRow);
+
+  function onChangeRow() {
+    var $this = $(this);
+    console.log('tt', $this.data('id'));
+  }
 
 });

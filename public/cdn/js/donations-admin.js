7d78f6968b958be1cdff3ef6e561f3810e1d1299
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2015-10-16 22:48:57 +0300

    update donations admin's page code to use object notation

--- public/cdn/js/donations-admin.js
+++ public/cdn/js/donations-admin.js
@@ -2,42 +2,65 @@ $(function () {
 
   $('#loadingContainer').fadeOut();
 
-  var table = $('#dataTable').DataTable({
-    fixedColumns: {
-      leftColumns: 2
+  var donationsOpts = {
+    fixedColsCount: 2,
+    url: 'mailchimp/get',
+    sortedColumnIndex: 1,
+  };
+
+  var donationsAdmin = {
+    mailchimpData: {},
+    init: function() {
+      this.bindEvents();
+      this.initDataTable();
     },
-    "order": [[ 1, "asc" ]],
-    "processing": true,
-    "scrollX": true,
-    "ajax": "mailchimp/get",
-    "columns": [
-      {
-        "mData": null,
-        "bSortable": false,
-        "mRender": function(data, type, full) {
-          return '<a class="fa fa-edit js-edit-row" data-id="' + data.id +'" href="javascript:void(0);"></a>';
-        }
-      },
-      {"data": "email"},
-      {"data": "merges.NAME_DONOR"},
-      {"data": "merges.COUNT"},
-      {"data": "merges.NOTE_DONOR"},
-      {"data": "merges.NOTE_ADMIN"},
-      {"data": "merges.ADDRESS"},
-      {"data": "merges.PHOTO"},
-      {"data": "merges.EMAIL_REAL"},
-      {"data": "merges.REGISTERED"},
-      {"data": "merges.RECEIVED"},
-      {"data": "merges.NAME_ADMIN"},
-      {"data": "merges.VALUE_USD"}
-    ]
-  });
+    bindEvents: function() {
+      $('#dataTable').on('click', '.js-edit-row', this.onChangeRow.bind(this));
+    },
+    onChangeRow: function(ev) {
+      var $this = $(ev.target);
+      console.log('Clicked ID:', $this.data('id'));
+    },
+    initDataTable: function() {
+      this.dataTable = $('#dataTable').DataTable({
+        fixedColumns: {
+          leftColumns: donationsOpts.fixedColsCount
+        },
+        "order": [[ donationsOpts.sortedColumnIndex, "asc" ]],
+        "processing": true,
+        "scrollX": true,
+        "ajax": {
+          url: donationsOpts.url,
+          dataSrc: function(d) {
+            this.mailchimpData = d.data;
+            return d.data;
+          }
+        },
+        "columns": [
+          {
+            "mData": null,
+            "bSortable": false,
+            "mRender": function(data) {
+              return '<a class="fa fa-edit js-edit-row" data-id="' + data.id +'" href="javascript:void(0);"></a>';
+            }
+          },
+          {"data": "email"},
+          {"data": "merges.NAME_DONOR"},
+          {"data": "merges.COUNT"},
+          {"data": "merges.NOTE_DONOR"},
+          {"data": "merges.NOTE_ADMIN"},
+          {"data": "merges.ADDRESS"},
+          {"data": "merges.PHOTO"},
+          {"data": "merges.EMAIL_REAL"},
+          {"data": "merges.REGISTERED"},
+          {"data": "merges.RECEIVED"},
+          {"data": "merges.NAME_ADMIN"},
+          {"data": "merges.VALUE_USD"}
+        ]
+      });
+    }
+  };
 
-  $('#dataTable').on('click', '.js-edit-row', onChangeRow);
-
-  function onChangeRow() {
-    var $this = $(this);
-    console.log('tt', $this.data('id'));
-  }
+  donationsAdmin.init();
 
 });

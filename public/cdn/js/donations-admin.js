43f50fe1a6fab69947ef19ae2fd796e2e7696689
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2015-10-17 22:25:51 +0300

    create simple search by id form for donations admin panel

--- public/cdn/js/donations-admin.js
+++ public/cdn/js/donations-admin.js
@@ -2,5 +2,27 @@ $(function () {
 
   $('#loadingContainer').fadeOut();
 
+  var idSearch = {
+    $form: $('#formSearchMailchimpId'),
+    init: function() {
+      this.bindEvents();
+    },
+    bindEvents: function() {
+      this.$form.on('submit', this.onFormSubmit.bind(this));
+    },
+    onFormSubmit: function(ev) {
+      ev.preventDefault();
+      var url = this.$form.attr('action');
+      var ajaxObj = $.get(url, this.$form.serialize());
+      ajaxObj.done(function(data) {
+        console.log('success', data);
+      });
+      ajaxObj.fail(function(err) {
+        console.log('error', err);
+      })
+    }
+  };
+
+  idSearch.init();
 
 });

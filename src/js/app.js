64b6df8fc22bd7d1b6485d81610c84be7995384d
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2017-09-16 18:53:01 +0300

    create animation for Map section

--- src/js/app.js
+++ src/js/app.js
@@ -10,5 +10,36 @@ $( document ).ready(function() {
     }, 300);
   });
 
+  // get all site names from html elements
+  var sites = getSitesNames();
+
+  function getSitesNames() {
+    var icons = $('.js-map-point');
+    return $.map(icons, function(icon){
+      return $(icon).attr('data-name');
+    });
+  }
+
+  function changeActiveSite(site) {
+    $('.js-map-point.active, .js-map-column.active').removeClass('active');
+    $('[data-name="' + site +'"]').addClass('active');
+  };
+
+  // function goes through all sites periodically and highlights one
+  function startChangeSiteInterval() {
+    var currentSiteIndex = 1;
+    setInterval(function(){
+      if (currentSiteIndex === sites.length) { currentSiteIndex = 0; }
+      changeActiveSite(sites[currentSiteIndex++]);
+    }, 5000);
+  }
+
+  if (sites.length) {
+    changeActiveSite(sites[0]);
+    if (sites.length > 1) {
+      startChangeSiteInterval();
+    }
+  }
+
 });
 

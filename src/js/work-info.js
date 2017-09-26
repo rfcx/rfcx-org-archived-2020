d2161735695c73f7c0e2b6022302ccdc52a9a6b7
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2017-09-26 21:48:48 +0300

    fill 3-cols block on homepage with text and links to Our Work page

--- src/js/work-info.js
+++ src/js/work-info.js
@@ -1,10 +1,21 @@
 "use strict";
 $( document ).ready(function() {
 
+  var hash;
+  if (window.location && window.location.hash) {
+    hash = window.location.hash.substring(1);
+  }
+  if (hash && hash.length) {
+    changeActiveHash(hash);
+  }
+
   $('.js-column-btn').on('click', function() {
     var name = $(this).attr('data-name');
-    $('.js-variable-content.active').removeClass('active');
-    $('.js-variable-content[data-name="' + name + '"]').addClass('active');
+    changeActiveHash(name);
   });
 
+  function changeActiveHash(name) {
+    $('.js-variable-content.active').removeClass('active');
+    $('.js-variable-content[data-name="' + name + '"]').addClass('active');
+  }
 });

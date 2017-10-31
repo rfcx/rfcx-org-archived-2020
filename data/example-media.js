91a08e8dded68d88a960ba3978c76c12052b4b0d
Moosecoop <cooper.makhijani@icloud.com>
2017-10-31 16:56:17 -0700

    cleaned up media.js and moved example to its own file

---
+++ data/example-media.js
@@ -0,0 +1,12 @@
+let data = {   id: "3d_model",
+       type: "link",
+       category: "device",
+       thmb: "",
+       orig_link: "http://p3d.in/90ZO0/xray+spin",
+       caption: {
+         english: "3D Model Link",
+         spanish: "",
+         french: ""
+       },
+       visible: true
+}//(,)
--- data/media.js
+++ data/media.js
@@ -1,9 +1,7 @@
 
 var data = [
-
-
-
-{   id: "device_stern_grove",
+  {
+    id: "device_stern_grove",
     type: "image",
     category: "device",
     thmb: "https://farm3.staticflickr.com/2895/14151071006_d2379544ec.jpg",
@@ -14,9 +12,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-{   id: "device_woodside",
+  },
+  {
+    id: "device_woodside",
     type: "image",
     category: "device",
     thmb: "https://farm6.staticflickr.com/5581/14466257085_81cb8b68a8_z.jpg",
@@ -27,9 +25,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-{   id: "device_redwood",
+  },
+  {
+    id: "device_redwood",
     type: "image",
     category: "device",
     thmb: "https://farm3.staticflickr.com/2904/13985834168_f0b94f5a1a_z.jpg",
@@ -40,16 +38,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-
-
-
-
-
-
-{   id: "device_v1_1",
+  },
+  {
+    id: "device_v1_1",
     type: "image",
     category: "installation",
     thmb: "https://farm8.staticflickr.com/7304/10834953145_102714f799_z.jpg",
@@ -60,9 +51,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-{   id: "device_v1_2",
+  },
+  {
+    id: "device_v1_2",
     type: "image",
     category: "installation",
     thmb: "https://farm4.staticflickr.com/3832/10834924195_383beff3d0_z.jpg",
@@ -73,9 +64,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-{   id: "device_v1_3",
+  },
+  {
+    id: "device_v1_3",
     type: "image",
     category: "installation",
     thmb: "https://farm6.staticflickr.com/5518/10835256143_5e7f6b88bd_z.jpg",
@@ -86,16 +77,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-
-
-
-
-
-
-{   id: "assembly_closeup",
+  },
+  {
+    id: "assembly_closeup",
     type: "image",
     category: "assembly",
     thmb: "https://farm4.staticflickr.com/3747/10834579143_18c9b19842_z.jpg",
@@ -106,10 +90,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-  
-{   id: "assembly_lean",
+  },
+  {
+    id: "assembly_lean",
     type: "image",
     category: "assembly",
     thmb: "https://farm3.staticflickr.com/2806/10834584413_43cf6172a5_z.jpg",
@@ -120,10 +103,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-{   id: "assembly_above",
+  },
+  {
+    id: "assembly_above",
     type: "image",
     category: "assembly",
     thmb: "https://farm8.staticflickr.com/7355/10834461514_b69e692158_z.jpg",
@@ -134,17 +116,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-
-
-
-
-
-
-
-{   id: "megafauna_siberut",
+  },
+  {
+    id: "megafauna_siberut",
     type: "image",
     category: "megafauna",
     thmb: "https://farm6.staticflickr.com/5500/10834081165_c75095a84c_z.jpg",
@@ -155,10 +129,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-  
-{   id: "megafauna_",
+  },
+  {
+    id: "megafauna_",
     type: "image",
     category: "megafauna",
     thmb: "https://farm3.staticflickr.com/2885/10834510913_7bf351a618_z.jpg",
@@ -169,10 +142,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-{   id: "megafauna_gibbon_vet",
+  },
+  {
+    id: "megafauna_gibbon_vet",
     type: "image",
     category: "megafauna",
     thmb: "https://farm3.staticflickr.com/2805/10834235526_66f6f7c1db_z.jpg",
@@ -183,10 +155,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-{   id: "megafauna_sloth",
+  },
+  {
+    id: "megafauna_sloth",
     type: "image",
     category: "megafauna",
     thmb: "https://farm3.staticflickr.com/2921/14304611749_8db1b61cb2_z.jpg",
@@ -197,10 +168,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-  
-{   id: "megafauna_beetle",
+  },
+  {
+    id: "megafauna_beetle",
     type: "image",
     category: "megafauna",
     thmb: "https://farm6.staticflickr.com/5585/14489945444_586a96f415_z.jpg",
@@ -211,10 +181,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-{   id: "valley_gorge",
+  },
+  {
+    id: "valley_gorge",
     type: "image",
     category: "megafauna",
     thmb: "https://farm6.staticflickr.com/5495/14468111226_7d53ea3002_z.jpg",
@@ -225,14 +194,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-
-
-
-
-{   id: "chainsaw_guy",
+  },
+  {
+    id: "chainsaw_guy",
     type: "image",
     category: "deforestation",
     thmb: "https://farm8.staticflickr.com/7333/10834112235_f30578a307_z.jpg",
@@ -243,16 +207,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-
-
-
-
-
-
-{   id: "infographic_how_it_works",
+  },
+  {
+    id: "infographic_how_it_works",
     type: "image",
     category: "illustration",
     thmb: "https://farm6.staticflickr.com/5593/14361735096_1cfc3b2125_z.jpg",
@@ -263,10 +220,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-  
-{   id: "deforestation_climate_change",
+  },
+  {
+    id: "deforestation_climate_change",
     type: "image",
     category: "illustration",
     thmb: "https://farm3.staticflickr.com/2940/14222374280_5783b48b6e_z.jpg",
@@ -277,10 +233,9 @@ var data = [
       french: ""
     },
     visible: true
-},
-
-
-{   id: "rfcx_3000_cars",
+  },
+  {
+    id: "rfcx_3000_cars",
     type: "image",
     category: "illustration",
     thmb: "https://farm6.staticflickr.com/5595/14489066484_13afe5fbe7_z.jpg",
@@ -291,29 +246,12 @@ var data = [
       french: ""
     },
     visible: true
-},
+  },
+  // syntax/example in example-media.js
 
+  ["", ""]];
 
-// {   id: "3d_model",
-//     type: "link",
-//     category: "device",
-//     thmb: "",
-//     orig_link: "http://p3d.in/90ZO0/xray+spin",
-//     caption: {
-//       english: "3D Model Link",
-//       spanish: "",
-//       french: ""
-//     },
-//     visible: true
-// },
-
-
-
-
-
-["",""]];
-
-exports.load = function() {
-  var rtrn = []; for (var i = 0; i < (data.length-1); i++) { rtrn[i] = data[i]; }
+exports.load = function () {
+  var rtrn = []; for (var i = 0; i < (data.length - 1); i++) { rtrn[i] = data[i]; }
   return rtrn;
 }

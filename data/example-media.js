1abd0cb6db050ca9da2c2e09af804fe1e3f9bedd
Moosecoop <cooper.makhijani@icloud.com>
2017-10-31 16:56:30 -0700

    cleaned up media.js and moved example to its own file

--- data/example-media.js
+++ data/example-media.js
@@ -1,12 +1,13 @@
-let data = {   id: "3d_model",
-       type: "link",
-       category: "device",
-       thmb: "",
-       orig_link: "http://p3d.in/90ZO0/xray+spin",
-       caption: {
-         english: "3D Model Link",
-         spanish: "",
-         french: ""
-       },
-       visible: true
+let data = {
+  id: "3d_model",
+  type: "link",
+  category: "device",
+  thmb: "",
+  orig_link: "http://p3d.in/90ZO0/xray+spin",
+  caption: {
+    english: "3D Model Link",
+    spanish: "",
+    french: ""
+  },
+  visible: true
 }//(,)

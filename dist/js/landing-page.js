dab95427cd7c4096f22f48345c41ccf676a55014
ska <sebastian.karg@quality-technology-solutions.de>
2017-12-12 08:03:50 +0100

    added functionality to video overlay

--- dist/js/landing-page.js
+++ dist/js/landing-page.js
@@ -10252,4 +10252,57 @@
 	return jQuery;
 	} );
 
-console.log("Hello from overlay.js");
+"use strict";
+
+var video = $('#ytplayer');
+var closeBtn = $('#overlay__close');
+var overlay = $('#overlay');
+
+// Hide overlay
+closeBtn.click(function() {
+  overlay.css({display: "none"});
+});
+
+
+
+//This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      //This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '349',
+          width: '560',
+          videoId: 'L0UK1GMtjig',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange,
+          }
+        });
+      }
+
+      //The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.pauseVideo();
+      }
+
+      //The API calls this function when the player's state changes.
+      //If the video is stopped (state = 0) the overlay will be shown 
+      function onPlayerStateChange(event) {
+          var state = player.getPlayerState();
+          console.log(state);
+          if( state === 0 ) {
+            overlay.css({display: "block"});
+          }
+        }
+        
+      function stopVideo() {
+        player.stopVideo();
+      }

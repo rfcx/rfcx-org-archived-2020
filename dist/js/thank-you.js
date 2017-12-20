2b97f6f46734394e52e0b40002311677a2fdfec8
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2017-12-20 17:33:57 +0300

    create timer function on thank-you page

--- dist/js/thank-you.js
+++ dist/js/thank-you.js
@@ -10290,3 +10290,30 @@ $( document ).ready(function() {
 //     'transactionTotal': 'REPLACE WITH VALUE' //value of the transaction
 //   });
 // }
+
+var timeout = 6;
+
+function timer() {
+  if (timeout > 1) {
+    decreaseTimeout();
+    refreshUI();
+    setTimeout(timer, 1000);
+  }
+  else {
+    console.log('redirect');
+  }
+}
+
+function decreaseTimeout() {
+  timeout--;
+}
+
+function refreshUI() {
+  document.querySelector('#secondsNumber').innerHTML = timeout;
+}
+
+function redirect() {
+  location.href = location.protocol + '//' + location.host;
+}
+
+timer();
